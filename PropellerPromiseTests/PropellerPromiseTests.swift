3aa9aa6a71abdbf8e3a0f1a0b5e124f3dc86fcf4
nadohs <djsente@gmail.com>
2017-02-02 17:18:10 -0800

    add CombinePromise w/ tests

--- PropellerPromiseTests/PropellerPromiseTests.swift
+++ PropellerPromiseTests/PropellerPromiseTests.swift
@@ -29,7 +29,7 @@ class PropellerPromiseTests: XCTestCase {
         return promise
     }
     
-    func testThenChainibngPromise() {
+    func testThenChainingPromise() {
         let expectation1 = self.expectation(description: "fire then string promise")
         let expectation2 = self.expectation(description: "fire then bool promise")
         let expectation3 = self.expectation(description: "fire then int promise")
@@ -43,7 +43,6 @@ class PropellerPromiseTests: XCTestCase {
             return isSuccess ? 1 : 0
         }
         .then { successInt -> Void in
-            print(successInt)
             expectation3.fulfill()
         }
         waitForExpectations(timeout: 1.0, handler: nil)
@@ -90,6 +89,7 @@ class PropellerPromiseTests: XCTestCase {
         }
         waitForExpectations(timeout: 1.0, handler: nil)
     }
+    
     func testAlwaysPromiseFailure() {
         let expectation1 = self.expectation(description: "fire on failure")
         let expectation2 = self.expectation(description: "fire always")
@@ -102,6 +102,96 @@ class PropellerPromiseTests: XCTestCase {
             }
             .always {
                 expectation2.fulfill()
+        }
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testCombinedPromiseSuccess() {
+        let expectation1 = self.expectation(description: "promise 1 finished")
+        let expectation2 = self.expectation(description: "promise 2 finished")
+        let expectation3 = self.expectation(description: "promise 3 finished")
+        let expectation4 = self.expectation(description: "combined proimse finished")
+        var p1Done = false
+        var p2Done = false
+        var p3Done = false
+        let p1 = successPromise()
+            .complete { value in
+                expectation1.fulfill()
+                p1Done = true
+            }
+            .failure { error in
+                XCTFail()
+            }
+        let p2 = successPromise()
+            .complete { value in
+                expectation2.fulfill()
+                p2Done = true
+            }
+            .failure { error in
+                XCTFail()
+        }
+        let p3 = successPromise()
+            .complete { value in
+                expectation3.fulfill()
+                p3Done = true
+            }
+            .failure { error in
+                XCTFail()
+        }
+        let _ = CombinePromise(promises: p1,p2,p3)
+            .complete {_ in
+                XCTAssert(p1Done)
+                XCTAssert(p2Done)
+                XCTAssert(p3Done)
+                expectation4.fulfill()
+            }
+            .failure { _ in
+                XCTFail()
+            }
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func testCombinedPromiseFailed() {
+        let expectation1 = self.expectation(description: "promise 1 finished")
+        let expectation2 = self.expectation(description: "promise 2 finished")
+        let expectation3 = self.expectation(description: "promise 3 finished")
+        let expectation4 = self.expectation(description: "combined proimse finished")
+        var p1Done = false
+        var p2Done = false
+        var p3Done = false
+        let p1 = successPromise()
+            .complete { value in
+                expectation1.fulfill()
+                p1Done = true
+            }
+            .failure { error in
+                XCTFail()
+        }
+        let p2 = successPromise()
+            .complete { value in
+                expectation2.fulfill()
+                p2Done = true
+            }
+            .failure { error in
+                XCTFail()
+        }
+        let p3 = failurePromise()
+            .complete { value in
+                XCTFail()
+                p3Done = true
+            }
+            .failure { error in
+                expectation3.fulfill()
+        }
+        CombinePromise(promises: p1,p2,p3)
+            .complete {_ in
+                XCTFail()
+            }
+            .failure { error in
+                XCTAssert(p1Done)
+                XCTAssert(p2Done)
+                XCTAssert(!p3Done)
+                expectation4.fulfill()
         }
         waitForExpectations(timeout: 1.0, handler: nil)
     }
